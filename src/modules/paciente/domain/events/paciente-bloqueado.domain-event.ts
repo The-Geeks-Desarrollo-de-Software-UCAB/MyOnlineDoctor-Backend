1caac962986a3fd66db97f5d6df72871aef73cf6
AnaZoe <50221238+AnaZoe@users.noreply.github.com>
2022-07-18 14:43:31 -0400

    Modificación Eventos Paciente

--- src/modules/paciente/domain/events/paciente-bloqueado.domain-event.ts
+++ src/modules/paciente/domain/events/paciente-bloqueado.domain-event.ts
@@ -1,9 +1,8 @@
 import { DomainEvent } from 'src/modules/base/domain/events/domain-event.base';
 
 export class PacienteBloqueadoDomainEvent extends DomainEvent {
-    private constructor(private readonly idAgg: string, private readonly _razon: string, private readonly _estadosuscripcion: string) {
+    private constructor(private readonly idAgg: string, private readonly _razon: string) {
         super(idAgg);
         this._razon = _razon;
-        this._estadosuscripcion = _estadosuscripcion;
     }
 }
--- src/modules/paciente/domain/events/suscripcion-pagada.domain-event.ts
+++ src/modules/paciente/domain/events/suscripcion-pagada.domain-event.ts
@@ -1,8 +1,8 @@
 import { DomainEvent } from 'src/modules/base/domain/events/domain-event.base';
 
 export class SuscripcionPagadaDomainEvent extends DomainEvent {
-    private constructor(private readonly idAgg: string, private readonly _estadosuscripcion: string) {
+    private constructor(private readonly idAgg: string) {
         super(idAgg);
-        this._estadosuscripcion = _estadosuscripcion;
+       
     }
 }
