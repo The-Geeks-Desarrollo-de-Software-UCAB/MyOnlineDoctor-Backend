3776d4156c4c68b1380c4600e0df27008b6785f2
AnaZoe <50221238+AnaZoe@users.noreply.github.com>
2022-07-19 22:23:47 -0400

    Logging en Archivo de Texto

--- src/modules/logging/console-logger.ts
+++ src/modules/logging/console-logger.ts
@@ -3,6 +3,16 @@ import { Logger } from './logger';
 export class ConsoleLogger implements Logger {
 
     public log(message: string): void {
+        var fs = require('fs');
+        fs.appendFile('LogSalida.txt',message + '\n',function(err){
+
+            if (err){
+                return console.log(err);
+            }
+            //console.log('Archivo creado');
+
+        });
+
         console.log(message);
     }
 
