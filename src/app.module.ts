0c95db111d705829ca42ca74d5b0d1e74830b8da
TESAX15 <tomassanchezacebey@gmail.com>
2022-07-20 00:54:13 -0400

    Arreglos para finalizar el merge de 2 ramas

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from 'src/app.controller';
 import { AppService } from 'src/app.service';
-import { CitaModule } from 'src/modules/cita/infrastructure/TypeORM/cita.module';
-import { DoctorModule } from 'src/modules/doctor/infrastructure/TypeORM/doctor.module';
-import { SpecialtyModule } from 'src/modules/doctor/infrastructure/TypeORM/specialty.module';
+import { CitaModule } from 'src/modules/cita/infrastructure/typeorm/cita.module';
+import { DoctorModule } from 'src/modules/doctor/infrastructure/typeorm/doctor.module';
+import { SpecialtyModule } from 'src/modules/doctor/infrastructure/typeorm/specialty.module';
 import { Cita } from 'src/modules/cita/infrastructure/typeorm/Entities/cita.entity';
-import { Especialidad } from 'src/modules/doctor/infrastructure/typeorm/Entities/specialty.entity';
-import { Paciente } from 'src/modules/paciente/infrastructure/typeorm/Entities/paciente.entity';
-import { Doctor } from 'src/modules/doctor/infrastructure/typeorm/Entities/doctor.entity';
+import { Especialidad } from 'src/modules/doctor/infrastructure/typeorm/entities/specialty.entity';
+import { Paciente } from 'src/modules/paciente/infrastructure/typeorm/entities/paciente.entity';
+import { Doctor } from 'src/modules/doctor/infrastructure/typeorm/entities/doctor.entity';
 
 @Module({
   imports: [
@@ -17,7 +17,7 @@ import { Doctor } from 'src/modules/doctor/infrastructure/typeorm/Entities/docto
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'Tesa15600*',
+      password: '', //ingresen aca su clave
       database: 'myonlinedoctor',
       entities: [Cita, Especialidad, Paciente, Doctor],
       autoLoadEntities: true,
